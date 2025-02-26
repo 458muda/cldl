b8a6e43007ac5343696c44a1b6dfb3e10120ae26
saivishal1996 <saivishalmuda@gmail.com>
2025-02-25 18:55:10 -0600

    code formatted and restructured

--- python_scripts/sandbox.py
+++ python_scripts/sandbox.py
@@ -1,22 +1,12 @@
 from osgeo import gdal
-from osgeo import gdal
-import sys
-import datetime
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from PIL import Image
-import random
-import cv2
 import pandas as pd
 from tensorflow.keras.utils import Sequence
 import albumentations as A
-import cv2
-import sklearn
 import segmentation_models
 import argparse
-import math
-
 
 from tensorflow.keras import optimizers
 #from keras.utils.np_utils import to_categorical
@@ -24,18 +14,11 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import *
+from tensorflow.keras.regularizers import l2
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras import backend as K
-
-
-
 import tensorflow as tf
 
-
-
-
-# band_path = 'test_data/bands/LC08_L1TP_118065_20180710_20180717_01_T1_band_chip260.tif'
-# label_path = 'test_data/labels/LC08_L1TP_118065_20180710_20180717_01_T1_label_chip260.tif'
 
 def read_bands(path):
     band_array = []
@@ -106,94 +89,112 @@ def binary_unet(input_height, input_width, bandNum):
     inputs = Input((input_height,input_width, bandNum))
     # Block one
     # (256, 256, numBands) -> (256, 256, 64)
-    conv1 = BatchNormalization()(Conv2D(32, 3, padding='same', name='Conv1_1', kernel_initializer='he_normal')(inputs))
+    conv1 = BatchNormalization()(Conv2D(32, 3, padding='same', name='Conv1_1', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(inputs))
     conv1 = Activation('relu')(conv1)
-    conv1 = BatchNormalization()(Conv2D(32, 3, padding='same', name='Conv1_2', kernel_initializer='he_normal')(conv1))
+    conv1 = BatchNormalization()(Conv2D(32, 3, padding='same', name='Conv1_2', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(conv1))
     conv1 = Activation('relu')(conv1)
     # (256, 256, 64) -> (128, 128, 64)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    pool1 = Dropout(0.2)(pool1)
+
 
     # Block Two
     # (128, 128, 64) -> (128, 128, 128)
-    conv2 = BatchNormalization()(Conv2D(64, 3, padding='same', name='Conv2_1', kernel_initializer='he_normal')(pool1))
+    conv2 = BatchNormalization()(Conv2D(64, 3, padding='same', name='Conv2_1', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(pool1))
     conv2 = Activation('relu')(conv2)
-    conv2 = BatchNormalization()(Conv2D(64, 3, padding='same', name='Conv2_2', kernel_initializer='he_normal')(conv2))
+    conv2 = BatchNormalization()(Conv2D(64, 3, padding='same', name='Conv2_2', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(conv2))
     conv2 = Activation('relu')(conv2)
     # (128, 128, 128) -> (64, 64, 128)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = Dropout(0.2)(pool2)
+
 
     # Block three
     # (64, 64, 128) -> (64, 64, 256)
-    conv3 = BatchNormalization()(Conv2D(128, 3, padding='same', name='Conv3_1', kernel_initializer='he_normal')(pool2))
+    conv3 = BatchNormalization()(Conv2D(128, 3, padding='same', name='Conv3_1', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(pool2))
     conv3 = Activation('relu')(conv3)
-    conv3 = BatchNormalization()(Conv2D(128, 3, padding='same', name='Conv3_2', kernel_initializer='he_normal')(conv3))
+    conv3 = BatchNormalization()(Conv2D(128, 3, padding='same', name='Conv3_2', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(conv3))
     conv3 = Activation('relu')(conv3)
     # (64, 64, 256) -> ( 32, 32, 256)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    pool3 = Dropout(0.3)(pool3)
+
 
     # Block four
     # (32, 32, 256) -> (32, 32, 512)
-    conv4 = BatchNormalization()(Conv2D(256, 3, padding='same', name='Conv4_1', kernel_initializer='he_normal')(pool3))
+    conv4 = BatchNormalization()(Conv2D(256, 3, padding='same', name='Conv4_1', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(pool3))
     conv4 = Activation('relu')(conv4)
-    conv4 = BatchNormalization()(Conv2D(256, 3, padding='same', name='Conv4_2', kernel_initializer='he_normal')(conv4))
+    conv4 = BatchNormalization()(Conv2D(256, 3, padding='same', name='Conv4_2', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(conv4))
     conv4 = Activation('relu')(conv4)
     # (32, 32, 512) -> (16, 16, 512)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    pool4 = Dropout(0.4)(pool4)
+
 
     # Block five
     # (16, 16, 512) -> (16, 16, 1024)
-    conv5 = BatchNormalization()(Conv2D(512, 3, padding='same', name='Conv5_1', kernel_initializer='he_normal')(pool4))
+    conv5 = BatchNormalization()(Conv2D(512, 3, padding='same', name='Conv5_1', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(pool4))
     conv5 = Activation('relu')(conv5)
-    conv5 = BatchNormalization()(Conv2D(512, 3, padding='same', name='Conv5_2', kernel_initializer='he_normal')(conv5))
+    conv5 = BatchNormalization()(Conv2D(512, 3, padding='same', name='Conv5_2', kernel_initializer='he_normal', kernel_regularizer=l2(0.01))(conv5))
     conv5 = Activation('relu')(conv5)
+    conv5 = Dropout(0.4)(conv5)
+
     
     ## Decoder 
     # Block six
     # (16, 16, 1024) -> (32, 32, 1024) -> (32, 32, 512)
-    up6 = Conv2DTranspose(256, (4, 4), strides=(2, 2), name = 'Conv6_1', padding='same')(conv5)
+    up6 = Conv2DTranspose(256, (4, 4), strides=(2, 2), name = 'Conv6_1', padding='same', kernel_regularizer=l2(0.01))(conv5)
     # (32, 32, 512) -> (32, 32, 1024)
-    merge6 = concatenate([conv4,up6],axis = 3)   
+    merge6 = concatenate([conv4,up6],axis = 3) 
+    merge6 = Dropout(0.4)(merge6)
+  
     # (32, 32, 1024) -> (32, 32, 512)
-    conv6 = (Conv2D(256, 3,  padding = 'same', name = 'Conv6_2', kernel_initializer = 'he_normal')(merge6))
+    conv6 = (Conv2D(256, 3,  padding = 'same', name = 'Conv6_2', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(merge6))
     conv6 = Activation('relu')(conv6) 
-    conv6 = (Conv2D(256, 3, padding = 'same', name = 'Conv6_3', kernel_initializer = 'he_normal')(conv6))
+    conv6 = (Conv2D(256, 3, padding = 'same', name = 'Conv6_3', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(conv6))
     conv6 = Activation('relu')(conv6) 
     
     # Block seven 
     # (32, 32, 512) -> (64, 64, 512) -> (64, 64, 256)
-    up7 = Conv2DTranspose(128, (4, 4), strides=(2, 2), name = 'Conv7_1', padding='same')(conv6)
+    up7 = Conv2DTranspose(128, (4, 4), strides=(2, 2), name = 'Conv7_1', padding='same', kernel_regularizer=l2(0.01))(conv6)
     # (64, 64, 256) -> (64, 64, 512)
     merge7 = concatenate([conv3,up7],axis = 3)
+    merge7 = Dropout(0.3)(merge7)
+
     # (64, 64, 512) -> (64, 64, 256)
-    conv7 = (Conv2D(128, 3,  padding = 'same', name = 'Conv7_2', kernel_initializer = 'he_normal')(merge7))
+    conv7 = (Conv2D(128, 3,  padding = 'same', name = 'Conv7_2', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(merge7))
     conv7 = Activation('relu')(conv7)          
-    conv7 = (Conv2D(128, 3, padding = 'same', name = 'Conv7_3', kernel_initializer = 'he_normal')(conv7))
+    conv7 = (Conv2D(128, 3, padding = 'same', name = 'Conv7_3', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(conv7))
     conv7 = Activation('relu')(conv7)  
                        
     # Block eight 
     # (64, 64, 256) -> (128, 128, 256) -> (128, 128, 128)
-    up8 = Conv2DTranspose(64, (4, 4), strides=(2, 2), name = 'Conv8_1', padding='same')(conv7)
+    up8 = Conv2DTranspose(64, (4, 4), strides=(2, 2), name = 'Conv8_1', padding='same', kernel_regularizer=l2(0.01))(conv7)
     # (128, 128, 128) -> (128, 128, 256)
-    merge8 = concatenate([conv2,up8],axis = 3)   
+    merge8 = concatenate([conv2,up8],axis = 3)
+    merge8 = Dropout(0.2)(merge8)
+ 
     # (128, 128, 256) -> (128, 128, 128)
-    conv8 = (Conv2D(64, 3, padding = 'same', name = 'Conv8_2', kernel_initializer = 'he_normal')(merge8))
+    conv8 = (Conv2D(64, 3, padding = 'same', name = 'Conv8_2', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(merge8))
     conv8 = Activation('relu')(conv8)                     
-    conv8 = (Conv2D(64, 3,  padding = 'same', name = 'Conv8_3', kernel_initializer = 'he_normal')(conv8))
+    conv8 = (Conv2D(64, 3,  padding = 'same', name = 'Conv8_3', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(conv8))
     conv8 = Activation('relu')(conv8)  
     
     # Block nine 
     # (128, 128, 128) -> (256, 256, 128) -> (256, 256, 64)
-    up9 = Conv2DTranspose(32, (4, 4), strides=(2, 2), name = 'Conv9_1', padding='same')(conv8)
+    up9 = Conv2DTranspose(32, (4, 4), strides=(2, 2), name = 'Conv9_1', padding='same', kernel_regularizer=l2(0.01))(conv8)
     # (256, 256, 64) -> (256, 256, 128)
     merge9 = concatenate([conv1,up9],axis = 3)
+    merge9 = Dropout(0.2)(merge9)
+
     # (256, 256, 128) -> (256, 256, 64)                  
-    conv9 = (Conv2D(32, 3, padding = 'same',name = 'Conv9_2', kernel_initializer = 'he_normal')(merge9))
+    conv9 = (Conv2D(32, 3, padding = 'same',name = 'Conv9_2', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(merge9))
     conv9 = Activation('relu')(conv9)                   
-    conv9 = (Conv2D(32, 3, padding = 'same', name = 'Conv9_3', kernel_initializer = 'he_normal')(conv9))
+    conv9 = (Conv2D(32, 3, padding = 'same', name = 'Conv9_3', kernel_initializer = 'he_normal', kernel_regularizer=l2(0.01))(conv9))
     conv9 = Activation('relu')(conv9)  
     
     # (256, 256, 64) -> (256, 256, 64)
-    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+    conv10 = Conv2D(1, 1, activation='sigmoid', kernel_regularizer=l2(0.01))(conv9)
     model = Model(inputs, outputs = conv10)
     
     return model
@@ -252,7 +253,7 @@ if __name__ == "__main__":
     val_data_csv = args.val_csv
     logdir = args.log_dir
     
-    os.environ["CUDA_VISIBLE_DEVICES"]= "0"
+    os.environ["CUDA_VISIBLE_DEVICES"]= "0,1,2,3"
     print(tf.test.is_gpu_available())
     print(tf.__version__)
     
@@ -276,22 +277,18 @@ if __name__ == "__main__":
     
     
 
-## two GPUs, so the devices are gpu0, and gpu1. If you try more GPUs, remember to change the devices.
-    #strategy = tf.distribute.MirroredStrategy(devices= ["/gpu:0","/gpu:1","/gpu:2","/gpu:3"],cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
-    #print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+# four GPUs, so the devices are gpu0, gpu1, gpu2, gpu3. to try more GPUs, remember to change the devices.
+    strategy = tf.distribute.MirroredStrategy(devices= ["/gpu:0","/gpu:1","/gpu:2","/gpu:3"],cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
+    print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
     
-    #with strategy.scope():
-    model_1 = binary_unet(256,256,6)
-    # print(model_1.summary())
+    with strategy.scope():
+        model_1 = binary_unet(256,256,6)
+        # print(model_1.summary())
 
-    model_1.compile(loss=loss,optimizer = adam_opt,metrics=['accuracy',segmentation_models.metrics.IOUScore()])
+        model_1.compile(loss=loss,optimizer = adam_opt,metrics=['accuracy',segmentation_models.metrics.IOUScore()])
 
 
-    model_1.fit(train_data,validation_data=val_data,epochs=500, callbacks=[tensorboard_callback, early_stop_callback, lr_callback])
-    
-    model_path = os.path.join("/home/vishal/DL_results", "unet64_lrc_500000_100"+".h5")
-    model_1.save(model_path)
-    
-    
-    
-
+        model_1.fit(train_data,validation_data=val_data,epochs=500, callbacks=[tensorboard_callback, early_stop_callback, lr_callback])
+        
+        model_path = os.path.join("/home/vishal/DL_results", "unet64_lrc"+".h5")
+        model_1.save(model_path)
